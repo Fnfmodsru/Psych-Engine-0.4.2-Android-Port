ae8c6fe0a665e05a06026001ed4ae7a32ded7757
Fnfmodsru <98064212+Fnfmodsru@users.noreply.github.com>
2022-01-26 14:22:42 +0900

    Add files via upload

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -59,8 +59,8 @@ class ChartingState extends MusicBeatState
 		'Hurt Note',
 		'GF Sing',
 		'No Animation'
-		'Ball Notes'
-		'Square Notes'
+		'Ball Notes',
+		'Square Notes',
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
